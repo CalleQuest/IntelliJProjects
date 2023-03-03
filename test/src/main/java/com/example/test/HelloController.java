b8bc126ca84767caea6c98d7deeb37ed387cc549
CalleQuest <questcalvin@dv-schulen.de>
2023-03-03 11:55:18 +0100

    nixgeht commit

--- test/src/main/java/com/example/test/HelloController.java
+++ test/src/main/java/com/example/test/HelloController.java
@@ -1,9 +1,11 @@
-package com.example.taschenrechner;
+package com.example.test;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
